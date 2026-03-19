bfab4c382338ec5e59ef8ae9b7b89abbf382033f
TavkirS <taukirofficial15@gmail.com>
2026-03-19 22:11:19 +0530

    I worked on year predictor method

---
+++ manjuPractise.py
@@ -0,0 +1,10 @@
+def yearPredictor(year):
+    if year == 2026:
+        print("This is Current Year")
+    elif year < 2026:
+        print("This is previous year")
+    else:
+        print("May be future year")
+
+
+yearPredictor(2027)
